0f59ffb65fbe8096fdcac34cc157324841bbe589
Trevor <tza0038@auburn.edu>
2022-10-04 02:40:44 -0500

    mobile support for footer

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -6,7 +6,15 @@ import {
   LocationOn,
   Mail,
 } from '@mui/icons-material'
-import { Box, Divider, Link, Typography, useTheme } from '@mui/material'
+import {
+  Box,
+  Divider,
+  Link,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2'
 
 import { routes, Link as RWLink } from '@redwoodjs/router'
 
@@ -50,151 +58,183 @@ const siteMapLinks: iconLink<() => string>[] = [
 
 const Footer = () => {
   const theme = useTheme()
+  const mobileMatch = useMediaQuery(theme.breakpoints.down('sm'))
+
   return (
-    <Box
+    <Grid
+      container
       component={'footer'}
-      sx={{
-        p: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        backgroundColor: theme.palette.auburnBlue.main,
-        width: '100%',
-        gap: 2,
-      }}
+      sx={{ p: 2, pb: 8, bgcolor: theme.palette.auburnBlue.main }}
     >
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          color: theme.palette.secondary.contrastText,
-        }}
-      >
-        <img
-          src="img/SquareLogo.png"
-          alt="Square CKAU Logo"
-          style={{
-            width: '100%',
-            maxWidth: '160px',
-            height: 'auto',
-          }}
-        />
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-          <Typography variant="h6" fontWeight="bold">
-            Contact Us
-          </Typography>
-          {contactUsLinks.map((link, idx) => {
-            return (
-              <Box sx={{ display: 'flex', gap: 1 }} key={idx}>
-                {link.icon}
+      <Grid xs={12}>
+        <Grid
+          container
+          columnSpacing={2}
+          rowGap={2}
+          sx={{ color: theme.palette.auburnBlue.contrastText }}
+        >
+          <Grid xs={12} sm={2}>
+            <img
+              src={mobileMatch ? 'img/RectangleLogo.png' : 'img/SquareLogo.png'}
+              alt="CKAU Logo"
+              style={{
+                width: '100%',
+                height: 'auto',
+              }}
+            />
+          </Grid>
+          <Grid xs={7} sm={3} smOffset={1}>
+            <Grid container rowGap={1}>
+              <Grid xs={12}>
+                <Typography variant="h6" fontWeight="bold">
+                  Contact Us
+                </Typography>
+              </Grid>
+              <Grid xs={12}></Grid>
+              <Grid container rowGap={1}>
+                {contactUsLinks.map((link, idx) => {
+                  return (
+                    <Grid xs={12} key={idx}>
+                      <Box sx={{ display: 'flex', gap: 1 }}>
+                        {link.icon}
+                        <Link
+                          href={link.href}
+                          target="_blank"
+                          rel="noopener"
+                          underline="hover"
+                        >
+                          {link.title}
+                        </Link>
+                      </Box>
+                    </Grid>
+                  )
+                })}
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={5} sm={3}>
+            <Typography variant="h6" fontWeight="bold">
+              Site Map
+            </Typography>
+            {siteMapLinks.map((link, idx) => {
+              return (
+                <Box sx={{ display: 'flex', gap: 1 }} key={idx}>
+                  {link.icon}
+                  <RWLink
+                    to={link.href()}
+                    style={{
+                      color: theme.palette.auburnBlue.contrastText,
+                      textDecoration: 'none',
+                    }}
+                  >
+                    <Typography
+                      sx={{ '&:hover': { textDecoration: 'underline' } }}
+                    >
+                      {link.title}
+                    </Typography>
+                  </RWLink>
+                </Box>
+              )
+            })}
+          </Grid>
+          <Grid xs={12} sm={3}>
+            <Grid container justifyContent="center">
+              <Grid xs={8}>
+                <Link href="https://auburn.edu/" target="_blank" rel="noopener">
+                  <img
+                    src="img/AULogo.png"
+                    alt="Auburn University Logo"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                    }}
+                  />
+                </Link>
+              </Grid>
+              <Grid xs={8}>
                 <Link
-                  href={link.href}
+                  href="https://auburn.campuslabs.com/engage/organization/thecampuskitchenatau"
                   target="_blank"
                   rel="noopener"
-                  underline="hover"
                 >
-                  {link.title}
+                  <img
+                    src="img/AUInvolveLogo.png"
+                    alt="Auburn Involve Logo"
+                    style={{
+                      width: '100%',
+                      height: 'auto',
+                    }}
+                  />
                 </Link>
-              </Box>
-            )
-          })}
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-          <Typography variant="h6" fontWeight="bold">
-            Site Map
-          </Typography>
-          {siteMapLinks.map((link, idx) => {
-            return (
-              <Box sx={{ display: 'flex', gap: 1 }} key={idx}>
-                {link.icon}
-                <RWLink
-                  to={link.href()}
-                  style={{
-                    color: theme.palette.auburnBlue.contrastText,
-                    textDecoration: 'none',
-                  }}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid xs={12}>
+        <Divider
+          sx={{
+            borderTopWidth: 2,
+            background: theme.palette.auburnOrange.main,
+          }}
+        />
+        <Grid
+          container
+          rowGap={2}
+          justifyContent="space-between"
+          sx={{
+            color: theme.palette.auburnBlue.contrastText,
+            textAlign: 'center',
+          }}
+        >
+          <Grid xs={12} sm={3}>
+            <Typography>
+              &copy; {new Date().getFullYear()} Campus Kitchen AU. All Rights
+              Reserved.
+            </Typography>
+          </Grid>
+          <Grid xs={12} sm={6}>
+            <Grid container justifyContent="center" columnSpacing={1}>
+              <Grid xs={12} sm={'auto'}>
+                <Typography>Proudly created by</Typography>
+              </Grid>
+              <Grid
+                xs={12}
+                sm={'auto'}
+                sx={{
+                  [theme.breakpoints.up('sm')]: { display: 'flex', gap: 1 },
+                }}
+              >
+                <Link
+                  href="https://www.linkedin.com/in/trevor-aupperle/"
+                  target="_blank"
+                  rel="noopener"
+                  underline="always"
+                  display="flex"
+                  justifyContent="center"
                 >
-                  <Typography
-                    sx={{ '&:hover': { textDecoration: 'underline' } }}
-                  >
-                    {link.title}
-                  </Typography>
-                </RWLink>
-              </Box>
-            )
-          })}
-        </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <Link href="https://auburn.edu/" target="_blank" rel="noopener">
-            <img
-              src="img/AULogo.png"
-              alt="Auburn University Logo"
-              style={{
-                width: '100%',
-                maxWidth: '192px',
-                height: 'auto',
-              }}
-            />
-          </Link>
-          <Link
-            href="https://auburn.campuslabs.com/engage/organization/thecampuskitchenatau"
-            target="_blank"
-            rel="noopener"
-          >
-            <img
-              src="img/AUInvolveLogo.png"
-              alt="Auburn Involve Logo"
-              style={{
-                width: '100%',
-                maxWidth: '192px',
-                height: 'auto',
-              }}
-            />
-          </Link>
-        </Box>
-      </Box>
-      <Divider
-        sx={{
-          borderTopWidth: 2,
-          background: theme.palette.auburnOrange.main,
-        }}
-      />
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          color: theme.palette.secondary.contrastText,
-        }}
-      >
-        <Typography>
-          &copy; {new Date().getFullYear()} Campus Kitchen AU. All Rights
-          Reserved.
-        </Typography>
-        <Typography alignItems="center" display="flex" gap="2px">
-          Proudly created by{' '}
-          <Link
-            href="https://www.linkedin.com/in/trevor-aupperle/"
-            target="_blank"
-            rel="noopener"
-            underline="always"
-            display="flex"
-          >
-            <LinkedIn /> Trevor Aupperle
-          </Link>
-          &
-          <Link
-            href="https://www.linkedin.com/in/ethan-wilkes/"
-            target="_blank"
-            rel="noopener"
-            underline="always"
-            display="flex"
-          >
-            <LinkedIn /> Ethan Wilkes.
-          </Link>
-        </Typography>
-        <Typography>Fork Hunger!</Typography>
-      </Box>
-    </Box>
+                  <LinkedIn /> Trevor Aupperle
+                </Link>
+                &
+                <Link
+                  href="https://www.linkedin.com/in/ethan-wilkes/"
+                  target="_blank"
+                  rel="noopener"
+                  underline="always"
+                  display="flex"
+                  justifyContent="center"
+                >
+                  <LinkedIn /> Ethan Wilkes.
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={12} sm={2}>
+            <Typography>Fork Hunger!</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   )
 }
 
