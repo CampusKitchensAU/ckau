81b982a03cab8d3198086483b0ccaf90535be0d3
Trevor <tza0038@auburn.edu>
2023-03-22 19:43:11 -0500

    Added Navbar to layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Navbar from "~/components/Navbar";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
