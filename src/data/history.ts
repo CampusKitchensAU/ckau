1fdbef1cd7a0938cb14cb6feb3620deba7e21d7e
Trevor <tza0038@auburn.edu>
2023-04-04 17:15:18 -0500

    Added history data and created history page layout.

---
+++ src/data/history.ts
@@ -0,0 +1,116 @@
+const history = [
+  {
+    id: 1,
+    path: "/history/2010-2011",
+    years: "Fall 2010 - Spring 2011",
+    title: "The beginning of Campus Kitchen",
+    summary:
+      "The Campus Kitchen was chartered as a student organization at Auburn University.",
+    description:
+      "June 16th, 2010, Auburn University submitted its paperwork to the national office, laying the groundwork for what would become The Campus Kitchens at Auburn University (CKAU). On March 1st, 2011, the organization was officially established with a signed contract, and operations began by serving 70 meals per week at the food pantry of Auburn United Methodist Church. These early efforts marked the beginning of CKAU's commitment to reducing food waste and fighting hunger in the East Central Alabama community.",
+  },
+  {
+    id: 2,
+    path: "/history/2013-2014",
+    years: "Fall 2013 - Spring 2014",
+    title: "Rewnewed growth",
+    summary:
+      "The Campus Kitchen was restored as an emerging student organization at Auburn University.",
+    description:
+      "This year marked a renewed growth of Campus Kitchens at Auburn University. Azeem Ahmed, a student at Auburn, traveled to Egypt in 2011 as a World Food Programme intern to help develop a national food program, conducting more than 40 field visits and certifying enough mills to produce 250,000 tons of vitamin-enriched rice. He saw hunger as an critical issue that was not just in these far-away countries, but also imminent in his own backyard. As a native of Auburn, he felt the responsibility to attend to hunger in the community with the incorporation of Campus Kitchens alongside Auburn's powerful Committee of 19, an organization affiliated with the United Nations World Food Programme.",
+    president: "Azeem Ahmed",
+  },
+  {
+    id: 3,
+    path: "/history/2014-2015",
+    years: "Fall 2014 - Spring 2015",
+    title: "CKP-AU Recognized for Sustainability Efforts",
+    summary:
+      "CKP-AU's efforts were recognized with the 2015 Spirit of Sustainability Award and included a school food backpack program and support for the establishment of other Campus Kitchens chapters.",
+    description:
+      "Then known as CKP-AU, the organization employed 7 community partners and was assisted by 800 volunteers for the year. Among its initiatives was a school food backpack program, which earned CKP-AU a national award. Additionally, the organization assisted in establishing two other Campus Kitchens chapters in other state universities, showcasing its commitment to food security beyond its local community. CKP-AU's efforts were recognized with the 2015 Spirit of Sustainability Award by the Office of Sustainability, further cementing its role in addressing food insecurity and promoting sustainable practices in the Auburn community.",
+    president: "Lauren Chastain",
+  },
+  {
+    id: 4,
+    path: "/history/2015-2016",
+    years: "Fall 2015 - Spring 2016",
+    title: "Growth and Outreach",
+    summary:
+      "The Campus Kitchens team at Auburn University saw remarkable growth in grants and outreach initiatives, including a crowd-funded campaign, grants, and new partnerships.",
+    description:
+      'During the 2015-2016 year, the Campus Kitchens team at Auburn University experienced significant growth and outreach efforts. The team raised $700 through a crowd-funded campaign called Raise the Dough and received a $1000 Sodexo Foundation Stephen J. Brady Stop Hunger Scholarship. They also raised $1000 to purchase a three-door cooler for the Community Market of the Food Bank of East Alabama. Through a Rural Hunger Outreach Grant, the team was able to expand their reach by incorporating Meals on Wheels in Auburn as a partner, growing the Community Market and Our House nutrition education programs, and establishing a "Hunger Fellow" to maintain summer operations. The Community Market guests were provided with nutrition information, recipes, and advice for preparing the fresh vegetables. The team also partnered with the Committee of 19 to serve students and residents of EASE House at a banquet funded in part by Tiger Dining. Additionally, they received the "Growing the Movement" award at the 2015 Food Waste & Hunger Summit held at the University of Georgia. During the year, the team served 11,109 meals to 15 community partners using 14,136 pounds of recovered food and a total of 1378 volunteer hours.',
+    president: "Daniel Cason",
+  },
+  {
+    id: 5,
+    path: "/history/2016-2017",
+    years: "Fall 2016 - Spring 2017",
+    title: '"Kitchen of the Year"',
+    summary:
+      'This year marked a significant expansion in the team\'s leadership and community partnerships, culminating in recognition at the Food Waste and Hunger Summit as the "Kitchen of the Year".',
+    description:
+      'The Campus Kitchens Project at Auburn University experienced substantial growth during the Fall 2016-Spring 2017 academic year. The number of shift leaders doubled from 15 to 30, helping to increase efficiency within operations. Additionally, 8 new community partners and 50 new committee members were added, with committees focused on outreach, nutrition, public relations, and operations. These efforts resulted in serving a total of 19,541 meals from 24,573 pounds of recovered food.\nThis expansion was recognized at the Food Waste and Hunger Summit, where the team earned the "Kitchen of the Year" Award for safe and efficient operations, community partnerships, participation in the Campus Kitchens Project network, and volunteer engagement. The team\'s dedication to reducing food waste and fighting hunger in the Auburn community was evident in their remarkable efforts during the academic year.',
+    president: "Hallie Nelson",
+  },
+  {
+    id: 6,
+    path: "/history/2017-2018",
+    years: "Fall 2017 - Spring 2018",
+    title: "Expanding Partnerships and Serving More Meals",
+    summary:
+      "Campus Kitchens at Auburn University continues to expand its community partnerships and increase food donations.",
+    description:
+      'During the 2017-2018 academic year, Campus Kitchens at Auburn University (CPK) continued to expand its partnerships and increase its impact in the community. With 13 community partners and a new partnership with Golden Corral restaurant for food collection, CPK was able to recover 33,924 pounds of food and serve 34,124 meals to those in need. Additionally, Publix Super Market Charities generously awarded CPK with $5,000, which helped to further support their efforts. The growing impact of CPK\'s work was recognized by the Campus Kitchens Project network, and their dedication to fighting food waste and hunger earned them the "Innovative Program of the Year" award at the 2018 Food Waste and Hunger Summit.',
+    president: "Ginny Lampkin",
+  },
+  {
+    id: 7,
+    path: "/history/2018-2019",
+    years: "Fall 2018 - Spring 2019",
+    title: "Serves over 53,000 meals",
+    summary:
+      "The Campus Kitchens Project at Auburn University experienced a revitalization in efficiency and recruitment, almost doubling the number of shift leaders and logging a total of 3,000 volunteer hours.",
+    description:
+      "During the Fall 2018 - Spring 2019 year, the Campus Kitchens Project at Auburn University experienced a significant increase in volunteer hours and leadership growth. The team saw a rise in the number of shift leaders from 27 to 42, almost doubling the previous year's count. This growth in leadership played a crucial role in their ability to serve 53,058 meals to 18 community partners, up from the previous year's 34,124 meals served. Additionally, the team logged a total of 3,000 volunteer hours, indicating a strong commitment to the cause of reducing food waste and feeding the hungry. The organization's continued efforts have provided invaluable support to their community, not only by recovering and serving food, but by inspiring volunteerism and leadership growth in the area.",
+    president: "Marina Sara",
+  },
+  {
+    id: 8,
+    path: "/history/2019-2020",
+    years: "Fall 2019 - Spring 2020",
+    title: "Resilience and Adaptation in the Face of Adversity",
+    summary:
+      "The Campus Kitchen Project at Auburn University persevered through various challenges, including the COVID-19 pandemic, and implemented new initiatives such as a recycling program and improved kitchen facilities.",
+    description:
+      "The Fall 2019 - Spring 2020 year was marked by the implementation of several new protocols and initiatives by the Campus Kitchen Project at Auburn University. These included a recycling initiative, making Family Meals more accessible, and fundraising efforts that resulted in the improvement of kitchen facilities. Unfortunately, the latter half of the year was significantly affected by the COVID-19 pandemic, leading to the suspension of operations upon the closure of the university. However, the team continued planning for the following year to ensure that shifts would be able to continue. Despite these challenges, the Campus Kitchen Project was able to serve a total of 35,568 meals from 39,117 pounds of recovered food to a record 27 community partners, demonstrating their resilience and adaptation in the face of adversity.",
+    president: "Michael Erwin",
+  },
+  {
+    id: 9,
+    path: "/history/2020-2021",
+    years: "Fall 2020 - Spring 2021",
+    title:
+      "Campus Kitchens at Auburn University perseveres amid pandemic challenges",
+    summary:
+      "Despite challenges posed by the COVID-19 pandemic, Campus Kitchens at Auburn University has continued its operations with effective leadership and safety measures to ensure the safety of volunteers and the community they serve.",
+    description:
+      "During this year CKAU was presented with unprecedented challenges, with the COVID-19 pandemic forcing shifts to reduce the number of volunteers and implement safety measures to maintain social distancing. However, the organization's leadership and dedicated volunteers have persevered in their mission to alleviate food insecurity in the Auburn-Opelika community. The organization's efforts to overcome these obstacles and continue its operations are a testament to its commitment to helping those in need.",
+    president: "William Illiano",
+  },
+  //   {
+  //     id: 10,
+  //     years: "Fall 2021 - Spring 2022",
+  //     title: "",
+  //     summary: "",
+  //     description: "",
+  //   },
+  //   {
+  //     id: 11,
+  //     years: "Fall 2022 - Spring 2023",
+  //     title: "",
+  //     summary: "",
+  //     description: "",
+  //   },
+];
+export default history;
--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,5 +1,32 @@
 import { type NextPage } from "next";
 import PageHead from "~/components/PageHead";
+import Image from "next/image";
+import history from "~/data/history";
+import Link from "next/link";
+import { MdChevronRight } from "react-icons/md";
+
+const images = [
+  {
+    src: "/images/history/history-1.jpg",
+    alt: "A group of volunteers preparing food",
+  },
+  {
+    src: "/images/history/history-2.jpg",
+    alt: "A group of volunteers preparing food",
+  },
+  {
+    src: "/images/history/history-3.jpg",
+    alt: "A group of volunteers preparing food",
+  },
+  {
+    src: "/images/history/history-4.jpg",
+    alt: "A group of volunteers preparing food",
+  },
+  {
+    src: "/images/history/history-5.png",
+    alt: "A group of volunteers preparing food",
+  },
+];
 
 const History: NextPage = () => {
   return (
@@ -8,6 +35,138 @@ const History: NextPage = () => {
         title="History"
         description="History page for The Campus Kitchen at Auburn University"
       />
+
+      <div className="bg-white py-8 md:py-14 lg:pt-24 lg:pb-16">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Our History
+            </h2>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              The Campus Kitchen at Auburn University has a rich history of
+              tackling food waste and hunger in East Central Alabama
+              communities. Our growth and success are a testament to the hard
+              work and dedication of our volunteers and the support of our
+              partners. Learn more about our journey and impact below.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-16 sm:mt-20">
+        <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+          {images.map((image, imageIndex) => (
+            <div
+              key={image.src}
+              className={`${
+                imageIndex == 1 || imageIndex == 4 ? "-rotate-2" : "rotate-2"
+              } relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-gray-100 sm:w-72 sm:rounded-2xl`}
+            >
+              <Image
+                src={image.src}
+                alt=""
+                sizes="(min-width: 640px) 18rem, 11rem"
+                className="absolute inset-0 h-full w-full object-cover"
+                fill
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="mx-auto max-w-7xl px-6 py-10 md:py-14 lg:py-20 lg:px-8">
+        <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+            <p className="flex-none text-3xl font-bold tracking-tight text-gray-900">
+              100+
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              <p className="text-lg font-semibold tracking-tight text-gray-900">
+                Dedicated volunteers
+              </p>
+              <p className="mt-2 text-base leading-7 text-gray-600">
+                Committed to leading regular weekly shifts for a semester.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-primary-700 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
+            <p className="flex-none text-3xl font-bold tracking-tight text-white">
+              153,000 meals
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              <p className="text-lg font-semibold tracking-tight text-white">
+                We&apos;re proud to say we&apos;ve delivered over 153,000 meals
+                to community members in need.
+              </p>
+              <p className="mt-2 text-base leading-7 text-gray-400">
+                Forking hunger, one meal at a time.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-secondary-300 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+            <p className="flex-none text-3xl font-bold tracking-tight text-white">
+              160,000 lbs
+            </p>
+            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+              <p className="text-lg font-semibold tracking-tight text-white">
+                Recovered food
+              </p>
+              <p className="mt-2 text-base leading-7 text-secondary-50">
+                Enabling us to make a meaningful impact by providing nutritious
+                meals while reducing food waste.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mx-auto max-w-2xl py-12 px-4 sm:px-8 lg:max-w-7xl lg:px-12">
+        <div className="md:border-l md:border-gray-100 md:pl-6">
+          <div className="flex max-w-3xl flex-col space-y-16">
+            {history.map((h) => (
+              <div
+                key={h.id}
+                className="md:grid md:grid-cols-4 md:items-baseline"
+              >
+                <div className="group relative flex flex-col items-start md:col-span-3">
+                  <h2 className="text-base font-semibold tracking-tight text-gray-800">
+                    <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-gray-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl" />
+                    <Link href={h.path}>
+                      <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
+                      <span className="relative z-10">{h.title}</span>
+                    </Link>
+                  </h2>
+                  <div className="relative z-10 order-first mb-3 flex items-center pl-3.5 text-sm text-gray-400 md:hidden">
+                    <span
+                      className="absolute inset-y-0 left-0 flex items-center"
+                      aria-hidden="true"
+                    >
+                      <span className="h-4 w-0.5 rounded-full bg-gray-200" />
+                    </span>
+                    {h.years}
+                  </div>
+                  <p className="relative z-10 mt-2 text-sm text-gray-500">
+                    {h.summary}
+                  </p>
+                  <div
+                    aria-hidden="true"
+                    className="relative z-10 mt-4 flex items-center text-sm font-medium text-secondary"
+                  >
+                    Learn more
+                    <MdChevronRight
+                      aria-hidden="true"
+                      className="ml-1 h-4 w-4 stroke-current"
+                    />
+                  </div>
+                </div>
+                <div className="relative z-10 order-first mt-1 mb-3 hidden items-center text-sm text-gray-400 md:block">
+                  {h.years}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
